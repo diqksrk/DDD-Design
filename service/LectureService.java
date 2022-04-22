8908454222d3295bf01a610d471af6121f71e036
diqksrk <diqksrk123@naver.com>
2022-04-22 21:09:14 +0900

    feat : lectureService 수정

--- service/LectureService.java
+++ service/LectureService.java
@@ -7,13 +7,21 @@ import java.util.List;
 
 public class LectureService {
 
-    public List<Lecture> lectures();
+    public HashMap<String, String> enrollemntLecture();
+
+    public HashMap<String, String> lectureContents();
+
+    public HashMap<String, String> evaluateLecture();
+
+    public HashMap<String, String> enrolledLectures();
+
+    public HashMap<String, String> manageLecture();
+
+    public HashMap<String, String> lectures();
 
     public HashMap<String, String> createLecture();
 
-    public HashMap<String, String> updateShowYnLecture();
-
-    public HashMap<String, String> showLecture();
+    public HashMap<String, String> updateVisibleLecture();
 
     public HashMap<String, String> giveStudentScore();
 
